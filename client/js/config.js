8fb24db85ef3df65620fd65cb92653114a669207
Attila Bulenda <36601162+attila-bulenda@users.noreply.github.com>
2025-04-30 23:16:50 +0200

    Moved URLs into a separate config file

---
+++ client/js/config.js
@@ -0,0 +1,9 @@
+const urls = {
+	base: "https://localhost:8080",
+	book: {
+		getBooks: "/api/books",
+		addBook: "/api/books",
+		removeBook: "/api/books/",
+		searchBookByIsbn: "/api/books/isbn/"
+	}
+}
